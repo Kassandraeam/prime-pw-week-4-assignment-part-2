e3bb2159982d4c7addb5252aeaadc21e1f874a59
Kas Eam <KassandraEam@gmail.com>
2022-06-28 17:50:45 -0500

    changed var to let

--- assignment/script.js
+++ assignment/script.js
@@ -1,5 +1,5 @@
 function changeImg() {
-    var image = document.getElementById('corolla');
+    let image = document.getElementById('corolla');
     if (image.src.match("https://cdn.jdpower.com/ChromeImageGallery/Expanded/Transparent/640/2018TOC040002_640/2018TOC040002_640_01.png")) {
         image.src = "https://www.mikeshawtoyota.com/2022-toyota-camry-in-corpus-christi-tx.htm";
     }
